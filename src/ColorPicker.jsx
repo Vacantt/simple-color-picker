db09a60448fdf3e002022e820074c0e011d23700
Marko Rufić <juongrudge3@gmail.com>
2025-02-20 21:07:58 +0100

    Changed default selected color to black

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 
 function ColorPicker() {
-  const [color, setColor] = useState('#FFFFFF');
+  const [color, setColor] = useState('#000000');
 
   function handleColorChange(event) {
     setColor(event.target.value);
